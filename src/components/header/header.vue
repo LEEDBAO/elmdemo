0ab31924f346959287f9597b412d3c2bd79c2194
lee <lee@smeinternet.com>
2019-03-15 17:58:48 +0800

    elmdemo37

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="seller" class="header">
-    <div  class="content-wrapper">
+    <div class="content-wrapper">
       <div class="avatar">
-        <img  :src="seller.avatar">
+        <img :src="seller.avatar">
       </div>
       <div class="content">
         <div class="title">
@@ -34,6 +34,20 @@
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
+          <div class="star-wrapper">
+            <star :size="36" :score="seller.score"></star>
+          </div>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul v-if="seller.supports" class="supports">
+            <li class="support-item" v-for="item in seller.supports" :key="item.id">
+              <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
+              <span class="text">{{seller.supports[$index].description}}</span>
+            </li>
+          </ul>
         </div>
       </div>
       <div class="detail-close">
@@ -44,6 +58,8 @@
 </template>
 
 <script>
+  import star from '../../components/star/star'
+
   export default {
     name: 'v-header',
     props: {
@@ -53,7 +69,7 @@
     },
     data () {
       return {
-          detailShow: false
+        detailShow: false
       }
     },
     methods: {
@@ -63,139 +79,212 @@
     },
     created () {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    },
+    components: {
+      star
     }
   }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus" >
+<style lang="stylus" rel="stylesheet/stylus">
   @import "../../../src/common/stylus/mixin.styl"
   .header
-    position :relative
+    position: relative
     color: #fff
     background: rgba(7, 17, 27, 0.5)
+
     .content-wrapper
-      position : relative
-      padding:24px 12px 18px 24px
-      font-size:0
+      position: relative
+      padding: 24px 12px 18px 24px
+      font-size: 0
+
       .avatar
         display: inline-block
-        vertical-align :top
+        vertical-align: top
+
         img
-          width:64px
-          height:64px
-          border-radius : 2px
+          width: 64px
+          height: 64px
+          border-radius: 2px
+
       .content
-        font-size:14px
+        font-size: 14px
         margin-left: 16px
         display: inline-block
+
         .title
           margin: 2px 0 8px 0
+
           .brand
             display: inline-block
-            width:30px
-            height:18px
+            width: 30px
+            height: 18px
             bg-image('brand')
-            background-size :30px 18px
+            background-size: 30px 18px
             background-repeat: no-repeat
-            vertical-align :top
+            vertical-align: top
+
           .name
             margin-left: 6px
             font-size: 16px
             line-height: 18px
-            font-weight :bold
+            font-weight: bold
+
           .description
             margin-bottom: 10px
             line-height: 12px
             font-size: 12px
+
         .support
           .icon
-            display :inline-block
-            width :12px
-            height : 12px
-            background-size : 12px 12px
-            background-repeat : no-repeat
+            display: inline-block
+            width: 12px
+            height: 12px
+            background-size: 12px 12px
+            background-repeat: no-repeat
             vertical-align: top
+
             &.decrease
               bg-image('decrease_1')
+
             &.discount
               bg-image('discount_1')
+
             &.guarantee
               bg-image('guarantee_1')
+
             &.invoice
               bg-image('invoice_1')
+
             &.special
               bg-image('special_1')
+
       .support-count
-        position : absolute
+        position: absolute
         right: 12px
-        height : 24px
+        height: 24px
         padding: 0 8px
         bottom: 18px
-        line-height :24px
-        border-radius :14px
-        background : rgba(0,0,0,0.2)
-        text-align : center
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, 0.2)
+        text-align: center
+
         .count
           vertical-align: top
-          font-size : 10px
+          font-size: 10px
+
         .icon-keyboard_arrow_right
           margin-left: 2px
           line-height: 24px
           font-size: 10px
+
     .bulletin-wrapper
-      position :relative
+      position: relative
       height: 28px
-      line-height :28px
+      line-height: 28px
       padding: 0 22px 0 12px
       white-space: nowrap
-      overflow :hidden
-      text-overflow : ellipsis
+      overflow: hidden
+      text-overflow: ellipsis
+
       .bulletin-title
-        vertical-align : top
-        margin-top :7px
+        vertical-align: top
+        margin-top: 7px
         display: inline-block
-        width : 22px
-        height : 12px
+        width: 22px
+        height: 12px
         bg-image('bulletin')
-        background-size : 22px 12px
-        background-repeat :no-repeat
+        background-size: 22px 12px
+        background-repeat: no-repeat
+
       .bulletin-text
-        vertical-align : top
-        margin :0 4px
-        font-size :10px
+        vertical-align: top
+        margin: 0 4px
+        font-size: 10px
+
       .icon-keyboard_arrow_right
-        position :absolute
-        font-size : 10px
-        right:12px
-        top:8px
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+
     .background-img
-      position : absolute
-      top:0
-      left:0
-      width :100%
-      height :100%
-      z-index :-1
-      filter: blur(5px)
-    .detail
-      position :fixed
-      z-index : 100
-      top:0
-      left:0
+      position: absolute
+      top: 0
+      left: 0
       width: 100%
       height: 100%
-      overflow:auto
+      z-index: -1
+      filter: blur(5px)
+
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
       background: rgba(7, 17, 27, 0.5)
+
       .detail-wrapper
-        min-height : 100%
-        width : 100%
+        min-height: 100%
+        width: 100%
+
         .detail-main
-          margin-top : 64px
+          margin-top: 64px
           padding-bottom: 64px
+
           .name
-            line-height :16px
-            text-align :center
-            font-size :16px
-            font-weight :700
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+          .star-wrapper
+            margin-top:18px
+            padding:2px 0
+            text-align:center
+          .title
+            display :flex
+            width :80%
+            margin: 30px auto 24px auto
+            .line
+              flex:1
+              position :relative
+              top: -6px
+              border-bottom :1px solid rgba(255,255,255,0.2)
+             .text
+              padding :0 12px
+              font-size : 14px
+              font-weight :700
+          .supports
+            width : 80%
+            margin : 0 auto
+            .support-item
+              padding:0 12px
+              margin-bottom: 12px
+              font-size:0
+              &:last-child
+                margin-bottom:0
+              .icon
+                display : inline-block
+                width : 16px
+                height :16px
+                vertical-align :top
+                margin-right : 6px
+                background-size : 16px 16px
+                background-repeat : no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
       .detail-close
         position: relative
         width: 32px
