5cc7aa4dad90b310dcd6b8d5c0b42c37c6cb25eb
lee <lee@smeinternet.com>
2019-03-18 14:53:54 +0800

    elmdemo39

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,30 +30,47 @@
     <div class="background-img">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail">
+    <!-- 加入渐变动画。加入fade -->
+    <transition name="fade">
+    <div v-show="detailShow" class="detail" transition="fade">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
           <div class="star-wrapper">
             <star :size="36" :score="seller.score"></star>
           </div>
+
           <div class="title">
             <div class="line"></div>
             <div class="text">优惠信息</div>
             <div class="line"></div>
           </div>
+
           <ul v-if="seller.supports" class="supports">
-            <li class="support-item" v-for="item in seller.supports" :key="item.id">
-              <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
-              <span class="text">{{seller.supports[$index].description}}</span>
+            <!-- 加入 v-for="(item, index) in items" 第二个参数为当前项的索引-->
+            <li class="support-item" v-for="(item,index) in seller.supports" :key="item.id">
+              <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+              <span class="text">{{seller.supports[index].description}}</span>
             </li>
           </ul>
+
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+
+          <div class="bulletin">
+            <p class="content">{{seller.bulletin}}</p>
+          </div>
         </div>
       </div>
-      <div class="detail-close">
+      <div class="detail-close" @click="closeDetail">
         <i class="icon-close"></i>
       </div>
+
     </div>
+    </transition>
   </div>
 </template>
 
@@ -75,6 +92,9 @@
     methods: {
       showDetail () {
         this.detailShow = true
+      },
+      closeDetail () {
+        this.detailShow = false
       }
     },
     created () {
@@ -226,7 +246,16 @@
       width: 100%
       height: 100%
       overflow: auto
-      background: rgba(7, 17, 27, 0.5)
+      backdrop-filter: blur(10px)
+      /* 渐变 */
+      opacity: 1
+      background: rgba(7, 17, 27, 0.8)
+      &.fade-enter-active,&.fade-leave-active
+        transition: all 0.5s
+      &.fade-enter, &.fade-leave-active,&.fade-leave-to
+        /*opacity 设置div元素不透明级别 */
+        opacity: 0
+        background: rgba(7, 17, 27, 0)
 
       .detail-wrapper
         min-height: 100%
@@ -241,50 +270,78 @@
             text-align: center
             font-size: 16px
             font-weight: 700
+
           .star-wrapper
-            margin-top:18px
-            padding:2px 0
-            text-align:center
+            margin-top: 18px
+            padding: 2px 0
+            text-align: center
+
           .title
-            display :flex
-            width :80%
+            display: flex
+            width: 80%
             margin: 30px auto 24px auto
+
             .line
-              flex:1
-              position :relative
+              flex: 1
+              position: relative
               top: -6px
-              border-bottom :1px solid rgba(255,255,255,0.2)
-             .text
-              padding :0 12px
-              font-size : 14px
-              font-weight :700
+              border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+
+            .text
+              padding: 0 12px
+              font-size: 14px
+              font-weight: 700
+
           .supports
-            width : 80%
-            margin : 0 auto
+            width: 80%
+            margin: 0 auto
+
             .support-item
-              padding:0 12px
+              padding: 0 12px
               margin-bottom: 12px
-              font-size:0
+              font-size: 0
+
               &:last-child
-                margin-bottom:0
+                margin-bottom: 0
+
               .icon
-                display : inline-block
-                width : 16px
-                height :16px
-                vertical-align :top
-                margin-right : 6px
-                background-size : 16px 16px
-                background-repeat : no-repeat
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+
                 &.decrease
                   bg-image('decrease_2')
+
                 &.discount
                   bg-image('discount_2')
+
                 &.guarantee
                   bg-image('guarantee_2')
+
                 &.invoice
                   bg-image('invoice_2')
+
                 &.special
                   bg-image('special_2')
+
+              .text
+                line-height: 16px
+                font-weight: 700
+                font-size: 12px
+
+          .bulletin
+            width: 80%
+            margin: 0 auto
+
+            .content
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
+
       .detail-close
         position: relative
         width: 32px
